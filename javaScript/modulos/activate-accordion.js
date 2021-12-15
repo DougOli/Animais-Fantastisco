0e3b08fc57b67b2726b91e89fa3ffa8a1b09b651
Douglas <douglas.oliveira@hammerconsult.com.br>
2021-12-15 14:35:37 -0300

    refatorando accordion

--- javaScript/modulos/activate-accordion.js
+++ javaScript/modulos/activate-accordion.js
@@ -1,19 +1,29 @@
-export default function activateAccordion() {
-  const accordionList = document.querySelectorAll(
-    '[data-anime="acorddion"] dt'
-  );
-  const ativo = "ativo";
-  function activeAccordion() {
-    this.classList.toggle(ativo);
-    this.nextElementSibling.classList.toggle(ativo);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.active = "ativo";
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(ativo);
-    accordionList[0].nextElementSibling.classList.add(ativo);
+  toggleAccordion(accordion) {
+    accordion.classList.toggle(this.active);
+    accordion.nextElementSibling.classList.toggle(this.active);
+  }
 
-    accordionList.forEach((accordion) => {
-      accordion.addEventListener("click", activeAccordion);
+  //  adiciona os eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((accordion) => {
+      accordion.addEventListener("click", () =>
+        this.toggleAccordion(accordion)
+      );
     });
+  }
+
+  //  iniciar funcao
+  init() {
+    if (this.accordionList.length) {
+      //  ativar primeiro item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
