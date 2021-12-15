7b680e635e5cf396cc99c0a5d92472e9faa32461
Douglas <douglas.oliveira@hammerconsult.com.br>
2021-12-15 15:04:04 -0300

    refatorando tabNav

--- javaScript/modulos/tab-animate.js
+++ javaScript/modulos/tab-animate.js
@@ -1,22 +1,31 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  const ativo = "ativo";
-
-  function activateTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove(ativo);
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add(ativo, direcao);
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.active = "ativo";
   }
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add(ativo);
 
-    tabMenu.forEach((item, index) => {
-      item.addEventListener("click", () => {
-        activateTab(index);
-      });
+  //  ativa a tab com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.active);
     });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.active, direcao);
+  }
+
+  //  adiciona os eventos ao TabNav
+  addTabNavEvent() {
+    this.tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      //  ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
