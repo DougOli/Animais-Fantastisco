70479429ae7fe7b7aea43e0076c4142deffd5863
Douglas <douglas.oliveira@hammerconsult.com.br>
2022-01-26 14:17:50 -0300

    refatorando modal

--- javaScript/modulos/modal.js
+++ javaScript/modulos/modal.js
@@ -1,19 +1,44 @@
-export default function initModal() {
-  const abrir = document.querySelector('[data-modal="abrir"]');
-  const fechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class Modal {
+  constructor(abrir, fechar, containerModal) {
+    this.abrir = document.querySelector(abrir);
+    this.fechar = document.querySelector(fechar);
+    this.containerModal = document.querySelector(containerModal);
 
-  function toggleModal(event) {
+    // bind diz ao callback parametros
+    // fazer referenciaa ao objeto de
+    // da classe
+
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
+  }
+  // abre e fecha o modal
+
+  toggleModal() {
+    this.containerModal.classList.toggle("ativo");
+  }
+
+  eventToggleModal(event) {
     event.preventDefault();
-    containerModal.classList.toggle("ativo");
+    this.toggleModal();
   }
 
-  function cliqueForaModal(event) {
-    if (event.target === this) toggleModal(event);
+  // Fecha o modal ao clicar do lado de fora
+
+  cliqueForaModal(event) {
+    if (event.target === this.containerModal) this.toggleModal(event);
   }
-  if (abrir && fechar && containerModal) {
-    abrir.addEventListener("click", toggleModal);
-    fechar.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", cliqueForaModal);
+  //  adiciona os eventos aos elementos do modal
+
+  addModalEvent() {
+    this.abrir.addEventListener("click", this.eventToggleModal);
+    this.fechar.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.cliqueForaModal);
+  }
+
+  init() {
+    if (this.abrir && this.fechar && this.containerModal) {
+      this.addModalEvent();
+    }
+    return this;
   }
 }
