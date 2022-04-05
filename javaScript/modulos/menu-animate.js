ad22074be7f03b8e9c8ce76f62b38b2ea4afca0f
Douglas <douglas.oliveira@hammerconsult.com.br>
2022-04-04 22:43:58 -0300

    criando novas funções para o anima scroll

--- javaScript/modulos/menu-animate.js
+++ javaScript/modulos/menu-animate.js
@@ -3,23 +3,40 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
     this.ativo = "ativo";
-    this.scrollAnimate = this.scrollAnimate.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  scrollAnimate() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.windowMetade < 0;
-      if (isSectionVisible) {
-        section.classList.add(this.ativo);
-      } else if (section.classList.contains(this.ativo)) {
-        section.classList.remove(this.ativo);
+  // Pega a distancia de cada item em relação ao topo
+  // do site
+
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica a distancia em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add(this.ativo);
+      } else if (item.element.classList.contains(this.ativo)) {
+        item.element.classList.remove(this.ativo);
       }
     });
   }
 
   init() {
-    this.scrollAnimate();
-    window.addEventListener("scroll", this.scrollAnimate);
+    if (this.sections.length) {
+      this.checkDistance();
+      this.getDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
   }
 }
